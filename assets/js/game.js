b3a5796f0ac5dd7bf67dcab9fe9edbc9ca1e2776
Aidan Amato <aidanamato@comcast.net>
2021-06-30 18:07:04 -0400

    begin coding game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name");
+
+console.log("This log is a string, good for leaving yourself a message!");
+console.log(10+10);
+window.alert("Your robot's name is "+playerName+"?");
+
+function fight() {
+    window.alert("The fight has begun!")
+}
+
+// fight();
